6520786842281572ce1c7f95c987a9cc145e551c
albonec <agbonecchi@gmail.com>
2023-05-15 14:18:46 -0700

    comments

--- src/Crosshair.java
+++ src/Crosshair.java
@@ -8,14 +8,14 @@ public class Crosshair {
     private int x;
     private int y;
 
-    public void setTargetPosition(int x, int y) { //Moves crosshair to position (x,y)
+    public void setTargetPosition(int x, int y) { // Moves crosshair to position (x,y)
         this.x = x;
         this.y = y;
     }
 
     public void render(Graphics2D g) {
-        g.setColor(new Color(0xFFFFFF)); //Sets crosshair color to white over black background
-        g.drawLine(x - 10, y, x + 10, y);
-        g.drawLine(x, y - 10, x, y + 10);
+        g.setColor(new Color(0xFFFFFF)); // Sets crosshair color to white over black background
+        g.drawLine(x - 10, y, x + 10, y); // Draws cross pattern
+        g.drawLine(x, y - 10, x, y + 10); // Draws cross pattern
     }
 }
